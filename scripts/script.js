5173c884c1499d25e9481b00f873edbcc121579b
Colin <wbdd.xyz@gmail.com>
2021-01-26 12:58:39 +0000

    added some styles, doctype, modal window functionality

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+
+'use strict';
+
+console.log(document.querySelector('.phone-number').textContent);
+
+
+
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const navbarEl = document.getElementById('.navbar');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.quote-button');
+
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  
+}
+
+for(let i = 0;i < btnOpenModal.length;i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+
+  const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+
+
+  btnCloseModal.addEventListener('click',closeModal);
+  overlay.addEventListener('click', closeModal);
+
+  document.addEventListener('keydown', function(e){
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+      closeModal();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
